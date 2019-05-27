10c6981e925a0836966ffc0b971d311ac384f44b
SXiaoL <46419181+SXiaoL@users.noreply.github.com>
2019-05-27 16:22:32 +0800

    dao

---
+++ src/main/java/com/yootk/dao/impl/MemberDAOImpl.java
@@ -0,0 +1,64 @@
+package com.yootk.dao.impl;
+
+import com.yootk.common.dao.abs.AbstractDAO;
+import com.yootk.dao.IMemberDAO;
+import com.yootk.vo.Member;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
+
+public class MemberDAOImpl extends AbstractDAO implements IMemberDAO {
+    @Override
+    public boolean doCreate(Member member) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean doEdit(Member member) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean doRemove(Set<String> strings) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Member findById(String s) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Member> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Member> findSplit(Long currentPage, Integer lineSize) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Member> findSplit(Long currentPage, Integer lineSize, String column, String keyWord) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Long getAllCount() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Long getAllCount(String column, String keyWord) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Member> findByIdAndPw(String mid) throws SQLException {
+        String sql = "SELECT mid,password FROM member WHERE mid=?";
+        super.pstmt = super.conn.prepareStatement(sql);
+        super.pstmt.setString(1,mid);
+        return null;
+    }
+}
---
+++ src/main/java/com/yootk/vo/Member.java
@@ -0,0 +1,141 @@
+package com.yootk.vo;
+
+import java.util.Date;
+
+public class Member implements java.io.Serializable {
+    private String mid;
+    private Long lid;
+    private Long did;
+    private String name;
+    private Double sal;
+    private String phone;
+    private String password;
+    private String photo;
+    private String note;
+    private Date regdate;
+    private String inmid;
+    private Integer locked;
+    private Integer type;
+    private String email;
+    private Long cuid;
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public Long getLid() {
+        return lid;
+    }
+
+    public void setLid(Long lid) {
+        this.lid = lid;
+    }
+
+    public Long getDid() {
+        return did;
+    }
+
+    public void setDid(Long did) {
+        this.did = did;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSal() {
+        return sal;
+    }
+
+    public void setSal(Double sal) {
+        this.sal = sal;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public String getInmid() {
+        return inmid;
+    }
+
+    public void setInmid(String inmid) {
+        this.inmid = inmid;
+    }
+
+    public Integer getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Integer locked) {
+        this.locked = locked;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCuid() {
+        return cuid;
+    }
+
+    public void setCuid(Long cuid) {
+        this.cuid = cuid;
+    }
+}
