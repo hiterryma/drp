6458bd53fa9bf223ed436e569817bc2e12976ded
WuXin <wuxin@git.com>
2019-05-27 16:49:50 +0800

    添加验证码检测

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -14,7 +14,7 @@ $(function(){
 			}
 			id = id + "Msg" ;
 			$("#" + id).empty() ;	// 先清除之前的所有内容
-			$("#" + id).append(error) ; 
+			$("#" + id).append(error) ;
 		},
 		highlight : function(element, errorClass) {
 			$(element).fadeOut(1,function() {
@@ -28,12 +28,12 @@ $(function(){
 			var id = $(element).attr("id") ;
 			id = id + "Msg" ;
 			$("#" + id).empty() ;	// 先清除之前的所有内容
-			$("#" + id).append("<span class='text-success glyphicon glyphicon-ok'></span>") ; 
+			$("#" + id).append("<span class='text-success glyphicon glyphicon-ok'></span>") ;
 		},
 		unhighlight : function(element, errorClass) {
 			$(element).fadeOut(1,function() {
 				$(element).fadeIn(1,function() {
-						$("#" + $(element).attr("id").replace(".","\\.") + "Div").attr("class","form-group has-success");
+					$("#" + $(element).attr("id").replace(".","\\.") + "Div").attr("class","form-group has-success");
 				});
 			})
 		},
@@ -42,13 +42,13 @@ $(function(){
 			"mid" : {
 				required : true
 			},
-			"password" : { 
+			"password" : {
 				required : true
 			} ,
 			"code" : {
 				required : true ,
 				remote : {
-					url : "CheckRandomActionFront!check.action", // 后台处理程序
+					url : "code_check.action", // 后台处理程序
 					type : "post", // 数据发送方式
 					dataType : "html", // 接收数据格式
 					data : { // 要传递的数据
@@ -60,6 +60,9 @@ $(function(){
 						if (data.trim() == "true") {
 							return true;
 						} else {
+							if ($("#code").val().length == 4) {
+								$("#imageCode").attr("src","ImageCode?p="+Math.random()) ;
+							}
 							return false;
 						}
 					}
