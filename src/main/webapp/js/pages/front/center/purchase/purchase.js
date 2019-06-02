25b4e7f847da198269262ac2527fd227b21b000b
SXiaoL <46419181+SXiaoL@users.noreply.github.com>
2019-06-02 20:13:45 +0800

    完善功能

--- src/main/webapp/js/pages/front/center/purchase/purchase.js
+++ src/main/webapp/js/pages/front/center/purchase/purchase.js
@@ -2,20 +2,22 @@ $(function() {
     $.getJSON("pages/front/center/purchase/get_purchase.action",function (data) {
         if (data == 1){
             $("#authentication>a").on('click', function(event) {
-                alert("您的客户认证已通过！");
                 event.preventDefault();
+                alert("您已通过客户认证！");
             });
         }
         if (data == 0){
-            $("#aaadd>a").on('click', function(event) {
-                alert("您还未进行客户认证申请或您的客户认证还在审核中，请耐心等待");
+            $("#purchaseRequest>a").on('click', function(event) {
                 event.preventDefault();
+                alert("您还未进行客户认证申请或您的客户认证还在审核中，请耐心等待");
+
             });
         }
         if (data == 3){
-            $("#aaadd>a").on('click', function(event) {
-                alert("您的客户认证审核未通过，请重新提交");
+            $("#purchaseRequest>a").on('click', function(event) {
                 event.preventDefault();
+                alert("您的客户认证审核未通过，请重新提交");
+
             });
         }
     });
