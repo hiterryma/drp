d82cd19191930ddc26bcebe39559f1391e9acd6d
SXiaoL <46419181+SXiaoL@users.noreply.github.com>
2019-06-03 16:20:24 +0800

    修复商品详情bug

--- src/main/webapp/js/pages/back/index.js
+++ src/main/webapp/js/pages/back/index.js
@@ -1,15 +1,16 @@
 $(function () {
    $.getJSON("/pages/back/member_message.action",function (date) {
+       console.log(date.photo);
        $("#imgae1").empty();
        $("#name1").empty();
        $("#imgae2").empty();
        $("#image3").empty();
 
-       $("#imgae1").append("<img src=\"http://43.226.146.219/upload/"+date.photo+"\" class=\"img-circle\" alt=\"User Image\">");
+       $("#imgae1").append("<img src=\"http://upload-server/upload/"+date.photo+"\" class=\"img-circle\" alt=\"User Image\">");
        $("#name1").append("<p>"+date.name+"</p>");
-       $("#imgae2").append("<img src=\"http://43.226.146.219/upload/"+date.photo+"\" class=\"user-image\" alt=\"User Image\">");
+       $("#imgae2").append("<img src=\"http://upload-server/upload/"+date.photo+"\" class=\"user-image\" alt=\"User Image\">");
        $("#name2").html(date.name);
-       $("#image3").append("<img src=\"http://43.226.146.219/upload/"+date.photo+"\" class=\"img-circle\" alt=\"User Image\">")
+       $("#image3").append("<img src=\"http://upload-server/upload/"+date.photo+"\" class=\"img-circle\" alt=\"User Image\">")
        $("#lasttime").html("上次登录日期："+date.lasttime)
    }) ;
 });
